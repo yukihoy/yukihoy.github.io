a4898590b57d350b6d4b647a6ba80b545f71d39a
yukihoy <blueberryykh@gmail.com>
2022-05-10 22:31:59 +0900

    必要ないので削除

--- master.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.getElementById("hamburgerMenu");
-const topNavMenu = document.getElementById("headerNavContainer");
-
-hamburgerMenu.addEventListener("click", () => {
-  topNavMenu.classList.toggle("menu-toggle");
-}
-);
